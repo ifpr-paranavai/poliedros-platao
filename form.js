25b4383c0f7b0c38eae0fefaea0a56cc3153654f
João Henrique S. Nunes <jh180403@gmail.com>
2024-02-22 15:58:05 -0300

    Substituição da verificação de formulário em PHP para JavaScript.

--- form.js
+++ form.js
@@ -8,125 +8,145 @@ $(document).ready(function () {
             nVertices: $("#" + event.originalEvent.target.id + " #nVertices").val(),
             somaAngs: $("#" + event.originalEvent.target.id + " #somaAngs").val(),
             forma: $("#" + event.originalEvent.target.id + " #forma").val(),
-            opni: $("#" + event.originalEvent.target.id + " #opni").val(),
-
-            //retorna qual botão de submit foi uzado
-            botao: event.originalEvent.submitter.name,
         };
 
-        //console.log(event.originalEvent.submitter.name);
 
-        $.ajax({
-            type: "POST",
-            url: "processa.php",
-            data: formData,
-            dataType: "json",
-            encode: true,
-        }).done(function (data) {
-            console.log(data);
+        //o codigo a seguir vai ficar meio estranho, pois se trata de uma adaptação/incorporação "direta" de uma parte do processamento
+        //que ocorria no PHP.
 
-            //garante que a munutenção de estados seja feita apenas no form que fez submit
-            $("#" + data.forma + "Form #nFaces").removeClass("is-invalid");
-            $("#" + data.forma + "Form #nFaces").removeClass("is-valid");
-            $("#" + data.forma + "Form #nFaces-group > #nFaces-erVaz").remove();
+        //garante que a munutenção de estados seja feita apenas no form que fez submit
+        $("#" + formData.forma + "Form #nFaces").removeClass("is-invalid");
+        $("#" + formData.forma + "Form #nFaces").removeClass("is-valid");
+        $("#" + formData.forma + "Form #nFaces-group > #nFaces-erVaz").remove();
+        $("#" + formData.forma + "Form #nFaces-group > #nFaces-crVaz").remove();
 
-            $("#" + data.forma + "Form #nArestas").removeClass("is-invalid");
-            $("#" + data.forma + "Form #nArestas").removeClass("is-valid");
-            $("#" + data.forma + "Form #nArestas-group > #nArestas-erVaz").remove();
+        $("#" + formData.forma + "Form #nArestas").removeClass("is-invalid");
+        $("#" + formData.forma + "Form #nArestas").removeClass("is-valid");
+        $("#" + formData.forma + "Form #nArestas-group > #nArestas-erVaz").remove();
+        $("#" + formData.forma + "Form #nArestas-group > #nArestas-crVaz").remove();
 
-            $("#" + data.forma + "Form #nVertices").removeClass("is-invalid");
-            $("#" + data.forma + "Form #nVertices").removeClass("is-valid");
-            $("#" + data.forma + "Form #nVertices-group > #nVertices-erVaz").remove();
+        $("#" + formData.forma + "Form #nVertices").removeClass("is-invalid");
+        $("#" + formData.forma + "Form #nVertices").removeClass("is-valid");
+        $("#" + formData.forma + "Form #nVertices-group > #nVertices-erVaz").remove();
+        $("#" + formData.forma + "Form #nVertices-group > #nVertices-crVaz").remove();
 
-            $("#" + data.forma + "Form #somaAngs").removeClass("is-invalid");
-            $("#" + data.forma + "Form #somaAngs").removeClass("is-valid");
-            $("#" + data.forma + "Form #somaAngs-group > #somaAngs-erVaz").remove();
+        $("#" + formData.forma + "Form #somaAngs").removeClass("is-invalid");
+        $("#" + formData.forma + "Form #somaAngs").removeClass("is-valid");
+        $("#" + formData.forma + "Form #somaAngs-group > #somaAngs-erVaz").remove();
+        $("#" + formData.forma + "Form #somaAngs-group > #somaAngs-crVaz").remove();
 
-            //altera os estados do form que fez o submit
-            if (data.botao == "enviar") {
-                console.log("enviar");
-                if (data.errors) {
-                    if (data.errors.nFaces) {
-                        $("#" + data.forma + "Form #nFaces").addClass("is-invalid");
-                        $("#" + data.forma + "Form #nFaces-group").append('<div class="invalid-feedback" id="nFaces-erVaz">' + data.errors.nFaces + "</div>");
-                    } else {
-                        $("#" + data.forma + "Form #nFaces").addClass("is-valid");
-                    }
+        
+        var resp = [];
 
-                    if (data.errors.nArestas) {
-                        $("#" + data.forma + "Form #nArestas").addClass("is-invalid");
-                        $("#" + data.forma + "Form #nArestas-group").append('<div class="invalid-feedback" id="nArestas-erVaz">' + data.errors.nArestas + "</div>");
-                    } else {
-                        $("#" + data.forma + "Form #nArestas").addClass("is-valid");
-                    }
+        //processamento das informações
+        switch (formData.forma) {
+            case "tetra":
+                resp = [4, 6, 4, 720];
+                break;
+            case "hexa":
+                resp = [6, 12, 8, 2160];
+                break;
+            case "octa":
+                resp = [8, 12, 6, 1440];
+                break;
+            case "dodec":
+                resp = [12, 30, 20, 6480];
+                break;
+            case "ico":
+                resp = [20, 30, 12, 3600];
+                break;
+        }
 
-                    if (data.errors.nVertices) {
-                        $("#" + data.forma + "Form #nVertices").addClass("is-invalid");
-                        $("#" + data.forma + "Form #nVertices-group").append('<div class="invalid-feedback" id="nVertices-erVaz">' + data.errors.nVertices + "</div>");
-                    } else {
-                        $("#" + data.forma + "Form #nVertices").addClass("is-valid");
-                    }
+        
+        var erros = [];
+        var acertos = [];
+        var data = [];
 
-                    if (data.errors.somaAngs) {
-                        $("#" + data.forma + "Form #somaAngs").addClass("is-invalid");
-                        $("#" + data.forma + "Form #somaAngs-group").append('<div class="invalid-feedback" id="somaAngs-erVaz">' + data.errors.somaAngs + "</div>");
-                    } else {
-                        $("#" + data.forma + "Form #somaAngs").addClass("is-valid");
-                    }
-                } else {
-                    $("#" + data.forma + "Form #nFaces").addClass("is-valid");
-                    $("#" + data.forma + "Form #nArestas").addClass("is-valid");
-                    $("#" + data.forma + "Form #nVertices").addClass("is-valid");
-                    $("#" + data.forma + "Form #somaAngs").addClass("is-valid");
-                }
-            }
-            else if (data.botao == "corrigir") {
-                console.log("corrigir");
+        //verifica se os campos estão preenchidos e/ou corretos
+        if (formData.nFaces != resp[0] && formData.nFaces !== "") {
+            erros['nFaces'] = 'Resposta incorreta.';
+        }else if(formData.nFaces == resp[0]){
+            acertos['nFaces'] = 'Resposta correta.';
+        }
 
-                if (data.errors) {
-                    if (data.errors.nFaces) {
-                        $("#" + data.forma + "Form #nFaces").addClass("is-invalid");
-                        $("#" + data.forma + "Form #nFaces-group").append('<div class="invalid-feedback" id="nFaces-erVaz">' + data.errors.nFaces + "</div>");
-                    }
+        if (formData.nArestas != resp[1] && formData.nArestas !== "") {
+            erros['nArestas'] = 'Resposta incorreta.';
+        }else if(formData.nArestas == resp[1]){
+            acertos['nArestas'] = 'Resposta correta.';
+        }
 
-                    if (data.errors.nArestas) {
-                        $("#" + data.forma + "Form #nArestas").addClass("is-invalid");
-                        $("#" + data.forma + "Form #nArestas-group").append('<div class="invalid-feedback" id="nArestas-erVaz">' + data.errors.nArestas + "</div>");
-                    }
+        if (formData.nVertices != resp[2]  && formData.nVertices !== "") {
+            erros['nVertices'] = 'Resposta incorreta.';
+        }else if(formData.nVertices == resp[2]){
+            acertos['nVertices'] = 'Resposta correta.';
+        }
 
-                    if (data.errors.nVertices) {
-                        $("#" + data.forma + "Form #nVertices").addClass("is-invalid");
-                        $("#" + data.forma + "Form #nVertices-group").append('<div class="invalid-feedback" id="nVertices-erVaz">' + data.errors.nVertices + "</div>");
-                    }
+        if (formData.somaAngs != resp[3] && formData.somaAngs !== "") {
+            erros['somaAngs'] = 'Resposta incorreta.';
+        }else if(formData.somaAngs == resp[3]){
+            acertos['somaAngs'] = 'Resposta correta.';
+        }
 
-                    if (data.errors.somaAngs) {
-                        $("#" + data.forma + "Form #somaAngs").addClass("is-invalid");
-                        $("#" + data.forma + "Form #somaAngs-group").append('<div class="invalid-feedback" id="somaAngs-erVaz">' + data.errors.somaAngs + "</div>");
-                    }
-                } 
+        
+        //armazena os dados a serem enviados de volta ao JavaScript
+        if (erros) {
+            data['success'] = false;
+            data['erros'] = erros;
+            data['forma'] = formData.forma;
+            data['acertos'] = acertos;
+        } else {
+            data['success'] = true;
+            data['message'] = 'Success!';
+            data['forma'] = formData.forma;
+            data['acertos'] = acertos;
+        }
 
-                if (data.acertos) {
-                    if (data.acertos.nFaces) {
-                        $("#" + data.forma + "Form #nFaces").addClass("is-valid");
-                    }
 
-                    if (data.acertos.nArestas) {
-                        $("#" + data.forma + "Form #nArestas").addClass("is-valid");
-                    }
-
-                    if (data.acertos.nVertices) {
-                        $("#" + data.forma + "Form #nVertices").addClass("is-valid");
-                    }
-
-                    if (data.acertos.somaAngs) {
-                        $("#" + data.forma + "Form #somaAngs").addClass("is-valid");
-                    }
-                }
+        //altera os estados do form que fez o submit
+        if (data['erros']) {
+            if (data['erros'].nFaces) {
+                $("#" + data['forma'] + "Form #nFaces").addClass("is-invalid");
+                $("#" + data['forma'] + "Form #nFaces-group").append('<div class="invalid-feedback" id="nFaces-erVaz">' + data['erros'].nFaces + "</div>");
             }
 
+            if (data['erros'].nArestas) {
+                $("#" + data['forma'] + "Form #nArestas").addClass("is-invalid");
+                $("#" + data['forma'] + "Form #nArestas-group").append('<div class="invalid-feedback" id="nArestas-erVaz">' + data['erros'].nArestas + "</div>");
+            }
 
+            if (data['erros'].nVertices) {
+                $("#" + data['forma'] + "Form #nVertices").addClass("is-invalid");
+                $("#" + data['forma'] + "Form #nVertices-group").append('<div class="invalid-feedback" id="nVertices-erVaz">' + data['erros'].nVertices + "</div>");
+            }
 
-        });
+            if (data['erros'].somaAngs) {
+                $("#" + data['forma'] + "Form #somaAngs").addClass("is-invalid");
+                $("#" + data['forma'] + "Form #somaAngs-group").append('<div class="invalid-feedback" id="somaAngs-erVaz">' + data['erros'].somaAngs + "</div>");
+            }
+        } 
+
+        if (data['acertos']) {
+            if (data['acertos'].nFaces) {
+                $("#" + data['forma'] + "Form #nFaces").addClass("is-valid");
+                $("#" + data['forma'] + "Form #nFaces-group").append('<div class="valid-feedback" id="nFaces-crVaz">' + data['acertos'].nFaces + "</div>");
+            }
+
+            if (data['acertos'].nArestas) {
+                $("#" + data['forma'] + "Form #nArestas").addClass("is-valid");
+                $("#" + data['forma'] + "Form #nArestas-group").append('<div class="valid-feedback" id="nArestas-crVaz">' + data['acertos'].nArestas + "</div>");
+            }
+
+            if (data['acertos'].nVertices) {
+                $("#" + data['forma'] + "Form #nVertices").addClass("is-valid");
+                $("#" + data['forma'] + "Form #nVertices-group").append('<div class="valid-feedback" id="nVertices-crVaz">' + data['acertos'].nVertices + "</div>");
+            }
+
+            if (data['acertos'].somaAngs) {
+                $("#" + data['forma'] + "Form #somaAngs").addClass("is-valid");
+                $("#" + data['forma'] + "Form #somaAngs-group").append('<div class="valid-feedback" id="somaAngs-crVaz">' + data['acertos'].somaAngs + "</div>");
+            }
+        }
+
         event.preventDefault();
     });
 });
